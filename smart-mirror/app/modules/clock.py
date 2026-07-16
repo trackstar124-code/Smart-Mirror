1e877bcdfaf2c80c9c2e192a0787b25fd72b3811
Vance Thorpe <thorpe.v@northeastern.edu>
2026-07-16 15:03:11 -0400

    Styled the Time and Date in the style.css file

--- smart-mirror/app/modules/clock.py
+++ smart-mirror/app/modules/clock.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 def get_time():
     now = datetime.now()
-    date = now.strftime("%Y-%m-%d")
+    date = now.strftime("%A, %B %d")
     time = now.strftime("%H:%M:%S")
     return {"date": date, "time": time}
 
